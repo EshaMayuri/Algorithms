7105d68005da042feffdcf2374a2833d6d696f45
emayuri <emayuri1990@gmail.com>
2017-12-30 11:43:36 -0600

    EVDS

---
+++ WebSourceCode/WebApplication/EmergencySystem.aspx.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication
+{
+    public partial class EmergencySystem : System.Web.UI.Page
+    {
+        public static List<Request> requests;
+        //public static Dictionary<int, DateTime> track = new Dictionary<int, DateTime>();
+        public Request request;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack && Session["EmergencySystem"] == null)
+            {
+                Session["Check_Page_Refresh"] = DateTime.Now.ToString();
+                requests = new List<Request>();
+                BindGridViewData();
+                Session["EmergencySystem"] = "EmergencySystem";
+
+                System.Threading.Thread deleteThread = new System.Threading.Thread(TrackRequest);
+                deleteThread.Start();
+            }
+            else if(!IsPostBack && Session["EmergencySystem"] != null)
+            {
+                Session["state"] = requests;
+                GridView1.DataSource = requests;
+                GridView1.DataBind();
+            }
+        }
+
+        /// <summary>
+        /// Function to track for how long a vehicle takes for servicing a request
+        /// </summary>
+        void TrackRequest()
+        {
+            //This will take 30 minutes
+            for (int i = 0; i < 360; i++)
+            {
+                if (requests.Count != 0 && requests[requests.Count - 1] != null)
+                {
+                    for(int j = 0; j< requests.Count; j++)
+                    {
+                        TimeSpan t = (DateTime.Now - requests[j].requestTime);
+                        if ( t.Minutes >= 2)
+                        {
+                            Delete(delete, EventArgs.Empty);
+                            //track.Remove(requestId);
+                        }
+                    }
+                }
+                System.Diagnostics.Debug.WriteLine("Task " + i + " - " + DateTime.Now);
+                System.Threading.Thread.Sleep(5000);
+            }
+        }
+
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            ViewState["Check_Page_Refresh"] = Session["Check_Page_Refresh"];
+        }
+
+        /// <summary>
+        /// Function to bind data to grid view
+        /// </summary>
+        private void BindGridViewData()
+        {
+            request = null;
+            requests.Add(null);
+
+            Session["state"] = requests;
+            GridView1.DataSource = requests;
+            GridView1.DataBind();
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GridView1_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName == "Delete")
+            {
+            }
+        }
+
+        /// <summary>
+        /// Function to delete a request once the processing time is crossed
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void Delete(object sender, EventArgs e)
+        {
+            VehicleDetails.Instance.VehicleUnSelected(requests[0].vehicleType, requests[0].zipCode, requests[0].vehicleId);
+            requests.RemoveAt(0);
+            
+            if (requests.Count == 0)
+            {
+                request = null;
+                requests.Add(null);
+            }
+            else
+            {
+                for (int i = 0; i < requests.Count; i++)
+                {
+                    requests[i].requestId = i + 1;
+                }
+            }
+            Session["state"] = requests;
+            Session["Check_Page_Refresh"] = DateTime.Now.ToString();
+            GridView1.DataSource = requests;
+            GridView1.DataBind();
+            ViewState["Check_Page_Refresh"] = Session["Check_Page_Refresh"];
+        }
+
+        /// <summary>
+        /// Insert a new request into list of requests maintained
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        public void Insert(object sender, EventArgs e)
+        {
+            if (ViewState["Check_Page_Refresh"].ToString() == Session["Check_Page_Refresh"].ToString())
+            {
+                int requestId = 0;
+                requests = new List<Request>();
+                requests = (List<Request>)Session["state"];
+
+                int vehicleType = ((DropDownList)GridView1.FooterRow.FindControl("ddlVehicleType")).SelectedIndex;
+                string zipCode = ((TextBox)GridView1.FooterRow.FindControl("txtZipCode")).Text;
+                if (vehicleType != 0 && !string.IsNullOrEmpty(zipCode))
+                {
+                    if (requests[requests.Count - 1] != null)
+                    {
+                        requestId = requests.Count() + 1;
+                    }
+                    else
+                    {
+                        requests = new List<Request>();
+                        requestId = 1;
+                    }
+                    request = new Request(requestId, (EmergencyVehicles)vehicleType, zipCode);
+                    if (request.Gap < int.MaxValue)
+                    {
+                        request.requestTime = DateTime.Now;
+                        requests.Add(request);
+                        //track.Add(request.requestId, DateTime.Now);
+                    }
+                    else
+                    {
+                        ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "alertMessage", "alert('Sorry! All the vehicles are currently attending other emergencies.')", true);
+                    }
+                    
+                }
+                Session["state"] = requests;
+                Session["Check_Page_Refresh"] = DateTime.Now.ToString();
+                GridView1.DataSource = requests;
+                GridView1.DataBind();
+                
+            }
+            else
+            {
+                requests = (List<Request>)Session["state"];
+                GridView1.DataSource = requests;
+                GridView1.DataBind();
+                Response.Write("Page Refresh Detected....");
+            }
+        }
+    }
+}
---
+++ WebSourceCode/WebApplication/VehicleDetails.cs
@@ -0,0 +1,309 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Xml;
+using System.IO;
+
+namespace WebApplication
+{
+    public sealed class VehicleDetails
+    {
+        //Vehicle type
+        public int type;
+        //Vehicle id
+        public int id;
+        //Zip code from where the request is made
+        public string zipCode;
+        Vehicle vehicle;
+
+        //Hash map to store vehicleType, zipCode and vehicleId's 
+        public Dictionary<EmergencyVehicles, Dictionary<string, List<int>>> vehicleMap;
+
+        //Hash map to store vehicle status(i.e. if the vehicle is already assigned to a request or not).
+        public Dictionary<int, Vehicle> vehicleStatus;
+
+        /// <summary>
+        /// Class constructor
+        /// </summary>
+        private VehicleDetails()
+        {
+            vehicleMap = new Dictionary<EmergencyVehicles, Dictionary<string, List<int>>>();
+            vehicleStatus = new Dictionary<int, Vehicle>();
+        }
+
+        private static VehicleDetails instance = null;
+
+        /// <summary>
+        /// Singleton class instance
+        /// </summary>
+        public static VehicleDetails Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new VehicleDetails();
+                }
+                return instance;
+            }
+        }
+
+
+
+        /// <summary>
+        /// Getter and Setter for Vehicle type
+        /// </summary>
+        public int Type
+        {
+            get
+            {
+                return type;
+            }
+            set
+            {
+                type = value;
+            }
+        }
+
+        /// <summary>
+        /// Getter and Setter for Zip code
+        /// </summary>
+        public string ZipCode
+        {
+            get
+            {
+                return zipCode;
+            }
+            set
+            {
+                zipCode = value;
+            }
+        }
+
+        /// <summary>
+        /// Getter and Setter for Vehicle id
+        /// </summary>
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+
+        /// <summary>
+        /// Function to fetch Vehicle Data from VehicleDetails.xml document
+        /// </summary>
+        public Dictionary<EmergencyVehicles, Dictionary<string, List<int>>> FetchVehicleData()
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            XmlNodeList xmlNodeList;
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+
+            FileStream fs = new FileStream(path+ @"\VehicleDetails.xml", FileMode.Open, FileAccess.Read);
+            xmlDoc.Load(fs);
+            xmlNodeList = xmlDoc.GetElementsByTagName("Vehicle");
+            for (int i = 0; i < xmlNodeList.Count; i++)
+            {
+                xmlNodeList[i].ChildNodes.Item(0).InnerText.Trim();
+                int id = int.Parse(xmlNodeList[i].ChildNodes.Item(0).InnerText.Trim());
+                EmergencyVehicles type = (EmergencyVehicles)int.Parse(xmlNodeList[i].ChildNodes.Item(1).InnerText.Trim());
+                string zipCode = xmlNodeList[i].ChildNodes.Item(2).InnerText.Trim().ToString();
+                PopulateVehicleDetails(type, zipCode, id);
+            }
+
+            fs.Close();
+            return vehicleMap;
+        }
+
+        /// <summary>
+        /// Function to populate vehicle details in the Hash Map for easy access.
+        /// </summary>
+        /// <param name="type">Vehicle Type</param>
+        /// <param name="zipCode">Zip code at which vehicle is available</param>
+        /// <param name="id">Vehicle id number</param>
+        public void PopulateVehicleDetails(EmergencyVehicles type, string zipCode, int id)
+        {
+            //Adding vehicles as per the type of the vehicle
+            //If vehicle type is Ambulance
+            if (type == EmergencyVehicles.Ambulance)
+            {
+                //If vehicle type is already available, new vehicle id will be concatinated to the list
+                if (vehicleMap.ContainsKey(type))
+                {
+                    //If zip code already has same vehicle type in it. the new vehicle id will be concatinated parallel to exisisting one
+                    if (vehicleMap[type].ContainsKey(zipCode))
+                    {
+                        List<int> ids = vehicleMap[type][zipCode];
+                        ids.Add(id);
+                        vehicleMap[type][zipCode] = ids;
+                        vehicle = new Vehicle(id, type, zipCode, true);
+                        vehicleStatus.Add(id, vehicle);
+                    }
+                    //Else new entry would be made for the zip code and vehicle will be added
+                    else
+                    {
+                        vehicleMap[type].Add(zipCode, new List<int> { id });
+                        vehicle = new Vehicle(id, type, zipCode, true);
+                        vehicleStatus.Add(id, vehicle);
+                    }
+                }
+                //If vehicle type is not available so far. New entry will be made.
+                else
+                {
+                    vehicleMap[type] = new Dictionary<string, List<int>>();
+                    vehicleMap[type].Add(zipCode, new List<int> { id });
+                    vehicle = new Vehicle(id, type, zipCode, true);
+                    vehicleStatus.Add(id, vehicle);
+                }
+            }
+            //If vehicle type is Fire Truck
+            else if (type == EmergencyVehicles.FireTruck)
+            {
+                if (vehicleMap.ContainsKey(type))
+                {
+                    if (vehicleMap[type].ContainsKey(zipCode))
+                    {
+                        List<int> ids = vehicleMap[type][zipCode];
+                        ids.Add(id);
+                        vehicleMap[type][zipCode] = ids;
+                        vehicle = new Vehicle(id, type, zipCode, true);
+                        vehicleStatus.Add(id, vehicle);
+                    }
+                    else
+                    {
+                        vehicleMap[type].Add(zipCode, new List<int> { id });
+                        vehicle = new Vehicle(id, type, zipCode, true);
+                        vehicleStatus.Add(id, vehicle);
+                    }
+                }
+                else
+                {
+                    vehicleMap[type] = new Dictionary<string, List<int>>();
+                    vehicleMap[type].Add(zipCode, new List<int> { id });
+                    vehicle = new Vehicle(id, type, zipCode, true);
+                    vehicleStatus.Add(id, vehicle);
+                }
+            }
+            //If vehicle type is Police car
+            else
+            {
+                if (vehicleMap.ContainsKey(type))
+                {
+                    if (vehicleMap[type].ContainsKey(zipCode))
+                    {
+                        List<int> ids = vehicleMap[type][zipCode];
+                        ids.Add(id);
+                        vehicleMap[type][zipCode] = ids;
+                        vehicle = new Vehicle(id, type, zipCode, true);
+                        vehicleStatus.Add(id, vehicle);
+                    }
+                    else
+                    {
+                        vehicleMap[type].Add(zipCode, new List<int> { id });
+                        vehicle = new Vehicle(id, type, zipCode, true);
+                        vehicleStatus.Add(id, vehicle);
+                    }
+                }
+                else
+                {
+                    vehicleMap[type] = new Dictionary<string, List<int>>();
+                    vehicleMap[type].Add(zipCode, new List<int> { id });
+                    vehicle = new Vehicle(id, type, zipCode, true);
+                    vehicleStatus.Add(id, vehicle);
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// Function to fetch list of specific emergency vehicles available across.
+        /// </summary>
+        /// <param name="vehicleType">Type of emergency vehicle</param>
+        /// <returns></returns>
+        public Dictionary<string, List<int>> FetchVehicleByType(EmergencyVehicles vehicleType)
+        {
+            Dictionary<string, List<int>> vehicleDetails = new Dictionary<string, List<int>>();
+            vehicleDetails = vehicleMap[vehicleType];
+            return vehicleDetails;
+        }
+
+        /// <summary>
+        /// Function to mark vehicle as selected
+        /// </summary>
+        /// <param name="vehicleType">Vehicle type</param>
+        /// <param name="zipCode">Zip code at which vehicle is available</param>
+        /// <param name="vehicleId">Vehicle id number</param>
+        /// <returns>returns boolean if vehicles status is changed</returns>
+        public bool VehicleSelected(EmergencyVehicles vehicleType, string zipCode, int vehicleId)
+        {
+            Dictionary<string, List<int>> vehicleDetails = new Dictionary<string, List<int>>();
+            vehicleDetails = vehicleMap[vehicleType];
+            List<int> vehicleIds = new List<int>();
+            vehicleIds = vehicleDetails[zipCode];
+            if (vehicleId != 0)
+            {
+                //vehicleStatus[vehicleId] = false;
+                vehicle = new Vehicle(vehicleId, vehicleType, zipCode, false);
+                vehicleStatus[vehicleId] = vehicle;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Function to mark vehicle as selected
+        /// </summary>
+        /// <param name="vehicleType">Vehicle type</param>
+        /// <param name="zipCode">Zip code at which vehicle is available</param>
+        /// <param name="vehicleId">Vehicle id number</param>
+        /// <returns>returns boolean if vehicles status is changed</returns>
+        public bool VehicleUnSelected(EmergencyVehicles vehicleType, string zipCode, int vehicleId)
+        {
+            //Dictionary<string, List<int>> vehicleDetails = new Dictionary<string, List<int>>();
+            //vehicleDetails = vehicleMap[vehicleType];
+            //List<int> vehicleIds = new List<int>();
+            //vehicleIds = vehicleDetails[zipCode];
+            if (vehicleId != 0)
+            {
+                //vehicleStatus[vehicleId] = false;
+                //vehicle = new Vehicle(vehicleId, vehicleType, zipCode, true);
+                vehicleStatus[vehicleId].status = true;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// Function to print vehicle details available in hash map
+        /// </summary>
+        public void PrintVehicleDetails()
+        {
+            Dictionary<string, List<int>> vehicleDetails = new Dictionary<string, List<int>>();
+            foreach (EmergencyVehicles type in vehicleMap.Keys)
+            {
+                vehicleDetails = vehicleMap[type];
+                foreach (string zip in vehicleDetails.Keys)
+                {
+                    List<int> vehicleIds = new List<int>();
+                    vehicleIds = vehicleDetails[zip];
+
+                    for (int k = 0; k < vehicleIds.Count; k++)
+                    {
+                        Console.WriteLine(vehicleIds[k]);
+                    }
+                }
+            }
+        }
+    }
+}
